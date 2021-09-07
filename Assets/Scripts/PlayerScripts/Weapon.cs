e05fb82d22ac306284dcd92df7cd777583e8a41b
dbonill <danbonilla002@gmail.com>
2021-09-07 16:39:32 -0500

    Hold To Shoot + Pause Menu Fix

--- Assets/Scripts/PlayerScripts/Weapon.cs
+++ Assets/Scripts/PlayerScripts/Weapon.cs
@@ -7,18 +7,24 @@ public class Weapon : MonoBehaviour
 
     public Transform firePoint;
     public GameObject bulletPrefab;
-    
+    public float delay = 0.2f;
+    float firingDelay = 0.0f;
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && OptionSettings.GameisPaused == false)
+        if (Input.GetButton("Fire1") && OptionSettings.GameisPaused == false && firingDelay < 0)
         {
             Shoot();
         }
-        
+        firingDelay -= Time.deltaTime;
+        if (firingDelay < -10000)
+            firingDelay = 0;
+
     }
 
     void Shoot()
     {
+        firingDelay = delay;
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
