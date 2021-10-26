8658a145d0884e123912a0076b4956338f61d01b
caoboyeehaw <dylanhoangcao@gmail.com>
2021-10-26 05:06:28 -0500

    Inefficient Enemy Counter + Minimap

---
+++ Assets/UnitCounterScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitCounterScript : MonoBehaviour
+{
+    GameObject[] enemies;
+    GameObject[] allies;
+    public Text allyCountText;
+    public Text enemyCountText;
+
+    private Image antibodyBar;
+
+    private float enemyLength;
+    private float allyLength;
+
+    private float totalUnits;
+    //public Image pathogenBar;
+
+    private float antibodyRatio;
+
+    private void Start()
+    {
+        antibodyBar = GameObject.Find("AntibodyAmountBar").GetComponent<Image>();
+        //pathogenBar = GameObject.Find("PathogenAmountBar").GetComponent<Image>();
+
+    }
+    void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        allies = GameObject.FindGameObjectsWithTag("Globin");
+
+       
+        enemyLength = float.Parse(enemies.Length.ToString());
+        allyLength = float.Parse(allies.Length.ToString());
+
+        enemyCountText.text = enemyLength.ToString();
+        allyCountText.text = allyLength.ToString();
+
+        totalUnits = allyLength + enemyLength;
+
+        antibodyBar.fillAmount = allyLength / totalUnits;
+    }
+}
