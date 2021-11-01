afc24e1dc26f4ec2767ee9a5946811b93999292f
caoboyeehaw <dylanhoangcao@gmail.com>
2021-11-01 09:26:28 -0500

    shop prefabs?

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
+    public GameObject shop;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            shop.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shop.SetActive(false);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
